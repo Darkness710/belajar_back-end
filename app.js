134b7ddb6b6e135b346510cf2cd593d4e78371a3
Darkness710 <133749753+Darkness710@users.noreply.github.com>
2023-10-12 16:37:24 +0000

    rill process.argv

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
-const memoryInformation = process.memoryUsage();
+const firstName = process.argv[2];
+const lastName = process.argv[3];
  
-console.log(memoryInformation);
- 
-/* output
-{
-  rss: 14569472,
-  heapTotal: 2654208,
-  heapUsed: 1788896,
-  external: 855681,
-  arrayBuffers: 9898
-}
-*/
+console.log(`Hello ${firstName} ${lastName}`);
