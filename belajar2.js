37b013d51b438b366e24e161e01bb4213a3ff1e6
Darkness710 <133749753+Darkness710@users.noreply.github.com>
2023-10-12 16:07:38 +0000

    process.memoryUsage()

---
+++ belajar2.js
@@ -0,0 +1,13 @@
+const memoryInformation = process.memoryUsage();
+ 
+console.log(memoryInformation);
+ 
+/* output
+{
+  rss: 14569472,
+  heapTotal: 2654208,
+  heapUsed: 1788896,
+  external: 855681,
+  arrayBuffers: 9898
+}
+*/
