fa74afa8009ad7209256f87985aab815a6ffeaeb
Darkness710 <133749753+Darkness710@users.noreply.github.com>
2023-10-12 16:32:45 +0000

    process.argv

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const server = new Server({
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'dicoding.com',
-});
+onst memoryInformation = process.memoryUsage();
+ 
+console.log(memoryInformation);
+ 
+/* output
+{
+  rss: 14569472,
+  heapTotal: 2654208,
+  heapUsed: 1788896,
+  external: 855681,
+  arrayBuffers: 9898
+}
+*/
